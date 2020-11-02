f7dce96091a255cda715a0d680342c2c9c4daa94
Maxim Vinogradov <maxim.vinogradov@fastdev.se>
2020-11-02 22:40:55 +0400

    add new activity and pass data to second activity

--- app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
+++ app/src/main/java/com/android/academy/fundamentals/MainActivity.kt
@@ -2,10 +2,33 @@ package com.android.academy.fundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.first_activity_text_view)
+        textView.setOnClickListener {
+            openSecondActivity()
+        }
+    }
+
+    private fun openSecondActivity() {
+        val intent = Intent(this, SecondActivity::class.java)
+
+        //Доп данные
+        val transmittedString = "string to transmit"
+        intent.putExtra(SecondActivity.TRANSMITTED_STRING, transmittedString)
+
+        val transmittedInt = 12
+        intent.putExtra(SecondActivity.TRANSMITTED_INT, transmittedInt)
+
+        val transmittedBool = false
+        intent.putExtra(SecondActivity.TRANSMITTED_BOOL, transmittedBool)
+
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/android/academy/fundamentals/SecondActivity.kt
+++ app/src/main/java/com/android/academy/fundamentals/SecondActivity.kt
@@ -2,10 +2,24 @@ package com.android.academy.fundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        var transmittedString: String? = intent.getStringExtra(TRANSMITTED_STRING)
+        var transmittedInt: Int = intent.getIntExtra(TRANSMITTED_INT, -1)
+        val transmittedBool : Boolean = intent.getBooleanExtra(TRANSMITTED_BOOL, false)
+
+        val textView: TextView = findViewById(R.id.second_activity_text_view)
+        textView.text = "Данные переданы с предыдущего экрана: transmittedString: $transmittedString, transmittedInt: $transmittedInt, transmittedBoolean: $transmittedBool"
+    }
+
+    companion object {
+        const val TRANSMITTED_STRING= "transmittedString"
+        const val TRANSMITTED_INT = "transmittedInt"
+        const val TRANSMITTED_BOOL = "transmittedBool"
     }
 }
